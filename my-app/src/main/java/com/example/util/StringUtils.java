a76f89d7d57f8c5f5fac75e7c0837fc0765fd579
UBenakana <udaykumarreddy27@gmail.com>
2024-05-27 23:49:03 +0530

    java app

---
+++ my-app/src/main/java/com/example/util/StringUtils.java
@@ -0,0 +1,11 @@
+package com.example.util;
+
+public class StringUtils {
+    public static String reverse(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
